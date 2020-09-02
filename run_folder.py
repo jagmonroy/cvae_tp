26ac543a6f4f1c28593a4dfdf6fc57b9a0bf5a1e
Jose Gallardo <jose.gallardo@cimat.mx>
2020-09-01 22:20:44 -0500

    before the readmes

--- run_folder.py
+++ run_folder.py
@@ -8,6 +8,7 @@ import os
 for config_file in os.listdir(sys.argv[1]):
 
 	if not 'yaml' in config_file: continue
+	if not 'sdvae' in config_file: continue
 
 	cf_path = os.path.join(sys.argv[1], config_file)
 	cmd1 = 'source ~/activate_env'
@@ -15,4 +16,5 @@ for config_file in os.listdir(sys.argv[1]):
 	cmd3 = 'CUDA_VISIBLE_DEVICES={0} python3 training.py {1}'
 	cmd3 = cmd3.format(sys.argv[2], cf_path)
 	cmd = 'ssh g-0-2 ' + '"' + " && ".join([cmd1, cmd2, cmd3]) + '"'
+	print(cmd)
 	os.system(cmd)
